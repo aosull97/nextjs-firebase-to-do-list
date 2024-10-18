98b28f9be1025fcdb3ac5ec7d4bea77763f53876
Alice <aosullivan@netpremacy.com>
2024-10-18 16:43:58 +0100

    create new task form

---
+++ src/app/components/NewTaskForm.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useState } from "react";
+import { collection, addDoc } from "firebase/firestore";
+import db from "../lib/firebase";
+
+const NewTaskForm = () => {
+    
+    const [value, setValue] = useState("");
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const docRef = await addDoc(collection(db, 'tasks'), {
+                title: value
+            });
+            console.log("Document written with ID: ", docRef.id);
+            alert('Task added!')
+            setValue(''); // Clear the form
+        } catch (e) {
+            console.error("Error adding document: ", e);
+        }
+    }   
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="m-auto w-1/4 p-5 mt-10 border rounded bg-gray-500">
+        <div>
+          <label className="mb-2 block">Title</label>
+          <input type="text" value={value} onChange={e => setValue(e.target.value)} className="text-gray-900"></input>
+        </div>
+        <button type="submit" className="p-2 border rounded hover:bg-slate-500 font-semibold mt-3">Add Task</button>
+      </form>
+    </div>
+  );
+};
+
+export default NewTaskForm;
