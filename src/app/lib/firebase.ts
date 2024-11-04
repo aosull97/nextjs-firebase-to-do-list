fd3827648d3517796f8a1380789393f3ff095158
Alice <aosullivan@netpremacy.com>
2024-11-04 12:06:18 +0000

    Configured .env.local variables

--- src/app/lib/firebase.ts
+++ src/app/lib/firebase.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
-//why does it not work when trying to use process.env variables?
 const firebaseConfig = {
-    apiKey: "AIzaSyDdBcWR_ta5y4xrkTZlatEsk4wvIMBs6Tc",
-    authDomain: "nextjs-task-manager-a8ca9.firebaseapp.com",
-    projectId: "nextjs-task-manager-a8ca9",
-    storageBucket: "nextjs-task-manager-a8ca9.appspot.com",
-    messagingSenderId: "100391853753",
-    appId: "1:100391853753:web:560e322b0e131bfdd34568"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
