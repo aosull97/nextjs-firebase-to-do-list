97eb0b21228bc5127e84eab29941c5e36d41c35d
Alice <aosullivan@netpremacy.com>
2024-11-05 16:23:14 +0000

    Added Logout button to homepage and made it a protected route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-import React, { useState } from 'react'
+'use client'
 import AddTaskButton from './components/AddTaskButton'
 import AllTasksList from './components/AllTasksList'
+import { useAuthState} from 'react-firebase-hooks/auth'
+import { auth } from './firebase/config'
+import { useRouter } from 'next/navigation'
+import { signOut } from 'firebase/auth'
 
 const ToDoList = () => {
 
+  const [user] = useAuthState(auth)
+  const router = useRouter()
+
+  if (!user) {
+    return router.push('/sign-in') //Adding return here stops the home page from flashing for a few seconds when the user is redirected
+  }
 
   return (
     <>
+      <div className='mt-4 mr-4 text-right'>
+        <button className='border rounded p-2 hover:bg-slate-500'
+         onClick={() => {
+          signOut(auth)
+        }}
+          >Logout</button>
+      </div>
       <div>
       <h1 className='text-center text-3xl font-bold underline mt-5'>My To Do List</h1>
       </div>
