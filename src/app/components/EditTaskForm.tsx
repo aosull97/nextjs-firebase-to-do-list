ef5893a49c5d591d9195ed25c76d801aad6f764c
Alice <aosullivan@netpremacy.com>
2024-11-07 11:56:16 +0000

    Added edit task functionality

--- src/app/components/EditTaskForm.tsx
+++ src/app/components/EditTaskForm.tsx
@@ -1,7 +1,7 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../firebase/config";
-import { doc, getDoc } from "@firebase/firestore";
-import { useState } from "react";
+import { doc, getDoc, setDoc, collection } from "@firebase/firestore";
+import { useEffect, useState } from "react";
 
 
 
@@ -9,32 +9,56 @@ const EditTaskForm = ({ taskId }: { taskId: string }) => {
   const [user] = useAuthState(auth);
   const [taskTitle, setTaskTitle] = useState("")
 
-  const fetchTaskData = async () => { 
-  const itemRef = doc(db, `users/${user?.email}/tasks/${taskId}`)
-  const taskDoc = await getDoc(itemRef)
-    if(!taskDoc.exists) {
-        console.log('No such document')
-    } else {
-        const data = taskDoc.data()
-        if(data){
-        console.log(data)
-        setTaskTitle(data.title)
+  useEffect(() => {
+    const fetchTaskData = async () => { 
+        const itemRef = doc(db, `users/${user?.email}/tasks/${taskId}`)
+        const taskDoc = await getDoc(itemRef)
+          if(!taskDoc.exists) {
+              console.log('No such document')
+          } else {
+              const data = taskDoc.data()
+              if(data){
+              console.log(data)
+              setTaskTitle(data.title)
+              }
+          }
         }
-    }
-  }
+        fetchTaskData()
+  
 
-  fetchTaskData()
+  }, [taskId])
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if(user?.email) { //Check if user.email is defined
+    try {
+        const taskDocRef = doc(db, `users/${user.email}/tasks/${taskId}`);
+        const docRef = await setDoc(taskDocRef, {
+            title: taskTitle
+        });
+        console.log(`Document with ID ${taskId} updated successfully}`);
+        alert('Task updated!')
+        setTaskTitle(taskTitle); //Set the form title to current one
+    } catch (e) {
+        console.error("Error updating document: ", e);
+    }
+}   else {
+  console.log("User email is undefined. Cannot add task")
+}
+};
 
 
   return (
     <div>
       <h1 className="text-gray-50 text-center text-2xl mt-10">Edit Task</h1>
       <div>
-        <form className="m-auto w-1/2 p-5 mt-10 rounded bg-gray-800 shadow-xl sm:w-1/4">
+        <form onSubmit={handleSubmit} className="m-auto w-1/2 p-5 mt-10 rounded bg-gray-800 shadow-xl sm:w-1/4">
           <div>
             <label className="mb-2 block text-gray-50">Task Name:</label>
             <input
-                defaultValue={taskTitle}
+            value={taskTitle} 
+            onChange={e => setTaskTitle(e.target.value)}
               type="text"
               className="w-full p-1 mb-4 bg-gray-700 rounded outline-none text-gray-100 placeholder-gray-500"
             ></input>
@@ -43,7 +67,7 @@ const EditTaskForm = ({ taskId }: { taskId: string }) => {
             type="submit"
             className="bg-orange-700 rounded text-gray-50 hover:bg-orange-600 p-2 mt-4"
           >
-            Add Task
+            Update Task
           </button>
         </form>
       </div>
