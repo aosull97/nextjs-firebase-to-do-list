9d26bb26ef5bd7f87e3e7d51dfa83f74beb2f1ec
Alice <aosullivan@netpremacy.com>
2024-11-08 10:30:22 +0000

    Added redirectToLogin to editTask page

--- src/app/editTask/[taskId]/page.tsx
+++ src/app/editTask/[taskId]/page.tsx
@@ -1,12 +1,22 @@
 'use client'
 import EditTaskForm from '@/app/components/EditTaskForm';
 import { useParams } from 'next/navigation';
-
+import RedirectToLogin from '@/app/redirect';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/app/firebase/config';
 
 const editTask = () => {
+  const [user] = useAuthState(auth)
   const params = useParams() as {taskId: string}
   const taskId = params.taskId;
-  console.log(taskId);
+
+
+  const ServerSideRedirect = () => {
+    if (!user) {
+      RedirectToLogin();
+    }
+    return null;
+  }
 
   if(!taskId) {
     return <div>Task not found</div>
@@ -14,6 +24,7 @@ const editTask = () => {
 
   return (
     <div>
+        <ServerSideRedirect />
         <EditTaskForm taskId={taskId}/>
     </div>
   )
